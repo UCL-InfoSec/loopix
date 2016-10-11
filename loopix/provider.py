acdfeada4e12cd3843ff1d3848cbd9a6479d5b57
Ania <aniapiotrowska2607@gmail.com>
2016-10-11 10:37:50 +0100

    Merge conflict solved

--- loopix/provider.py
+++ loopix/provider.py
@@ -42,11 +42,7 @@ class Provider(MixNode):
         self.d.addCallback(self.turnOnHeartbeats)
         self.d.addErrback(self.errbackHeartbeats)
         self.turnOnReliableUDP()
-#<<<<<<< HEAD
         #self.saveInDB('example.db')
-#=======
-        # self.saveInDB('example.db')
-#>>>>>>> fa544555cba87059f3fbadf3d4506969f4ce4003
 
     def stopProtocol(self):
         print "[%s] > Stop protocol" % self.name
