43885b5a8f01f5468683ddd26e29f37acc57e5ac
Ania <aniapiotrowska2607@gmail.com>
2016-10-18 13:01:20 +0100

    Small provider update

--- loopix/provider.py
+++ loopix/provider.py
@@ -94,6 +94,7 @@ class Provider(MixNode):
             print "[%s] > provider received assign message from client (%s, %d)" % (self.name, host, port)
             log.info("[%s] > provider received assign message from client (%s, %d)" % (self.name, host, port))
             def do_SUBSCRIBE(IPAddr):
+                print "Host, Resolved: " % (host,IPAddr)
                 self.subscribeClient(data[4:], IPAddr, port)
             reactor.resolve(host).addCallback(do_SUBSCRIBE)
 
