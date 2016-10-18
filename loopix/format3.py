87a6d6c362710f973aca2e0d893004ce6ec38d52
Ania <aniapiotrowska2607@gmail.com>
2016-10-18 18:25:32 +0100

    Format file update

--- loopix/format3.py
+++ loopix/format3.py
@@ -135,7 +135,6 @@ def create_mixpacket_format(sender, receiver, mixes, setup, dest_message='', ret
 	# Build the backward path
 	msgback = return_message
 	for j in range(len(mixes) + 1):
-		print all_data[j]
 		mix, bs, yelem, (mixfrom, mixto), (kforw, kback) = all_data[j]
 		the_bs = bs.mod_inverse(o)
 		delay_i = (delay[j] if delay else 0)
