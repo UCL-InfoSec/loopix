716266690b004c6ad60a9157c573f9c3ed4869a0
Ania <aniapiotrowska2607@gmail.com>
2016-10-19 16:38:23 +0100

    Remote client setup update

--- loopix/setup_client.py
+++ loopix/setup_client.py
@@ -9,20 +9,20 @@ import os.path
 
 if __name__ == "__main__":
 
-	if not (os.path.exists("secretClient.prv") or os.path.exists("publicClient.bin")):
+	port = int(sys.argv[1])
+	host = sys.argv[2]
+	name = sys.argv[3]
+	prvname = sys.argv[4]
 
-		port = int(sys.argv[1])
-		host = sys.argv[2]
-		name = sys.argv[3]
-		prvname = sys.argv[4]
+	if not (os.path.exists("secretClient%s.prv"%name) or os.path.exists("publicClient%s.bin"%name)):
 
 		setup = format3.setup()
 		G, o, g, o_bytes = setup
 
 		secret = o.random()
-		file("secretClient.prv", "wb").write(petlib.pack.encode(secret))
+		file("secretClient%s.prv"%name, "wb").write(petlib.pack.encode(secret))
 
 		pub = secret * g
-		file("publicClient.bin", "wb").write(petlib.pack.encode(["client", name, port, host, pub, prvname]))
+		file("publicClient%s.bin"%name, "wb").write(petlib.pack.encode(["client", name, port, host, pub, prvname]))
 	else:
 		print "Files exist"
