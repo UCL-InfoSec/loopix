a60d8735a1993137bac93b69c8b3b3f5d05cf9d7
Ania <aniapiotrowska2607@gmail.com>
2016-11-06 15:58:04 +0000

    PQ params update

--- loopix/processQueue.py
+++ loopix/processQueue.py
@@ -14,9 +14,9 @@ class ProcessQueue():
 
 		self.target = 0.5
 
-		self.Kp = 10.0 #2
+		self.Kp = 8.0 #2
 		self.Ki = 6.5 #1
-		self.Kd = 8.0 #5
+		self.Kd = 15.0 #5
 
 		self.drop = 0
 		self.sum_Error = 0.0
