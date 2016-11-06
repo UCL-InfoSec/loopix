4d8a6945cb12a4b5b3989e1ffab7813851e33983
Ania <aniapiotrowska2607@gmail.com>
2016-11-06 22:46:46 +0000

    PQ process measure

--- loopix/processQueue.py
+++ loopix/processQueue.py
@@ -15,7 +15,7 @@ class ProcessQueue():
 		self.target = 0.5
 
 		self.Kp = 3.0 #2
-		self.Ki = 3.0 #1
+		self.Ki = 1.0 #1
 		self.Kd = 10.0 #5
 
 		self.drop = 0
@@ -59,7 +59,8 @@ class ProcessQueue():
 		P = self.timings - self.target
 
 		# the contribution from the integral term is proportional to both the magnitude of the error and the duration of the error
-		I = 0.8 * self.sum_Error + 0.2 * P # the integral in a PID controller is the sum of the instantaneous error over time and gives the accumulated offset that should have been corrected previously
+		# I = 0.8 * self.sum_Error + 0.2 * P # the integral in a PID controller is the sum of the instantaneous error over time and gives the accumulated offset that should have been corrected previously
+		I  = self.sum_Error + P
 
 		# Derivative action predicts system behavior and thus improves settling time and stability of the system
 		#D = P - I # the derivative of the process error is calculated by determining the slope of the error over time
