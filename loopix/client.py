2ee6c4c4e110496b6aba47532a7b8f1bce552ebd
Ania <aniapiotrowska2607@gmail.com>
2016-11-10 12:21:46 +0000

    Small update

--- loopix/client.py
+++ loopix/client.py
@@ -579,7 +579,7 @@ class Client(DatagramProtocol):
             print "[%s] > ERROR: %s" % (self.name, str(e))
 
     def measureLatency(self, msg, providerTimestamp):
-        # print ">TAG MESSAGE RECEIVED: This is a taged message, to measure latency"
+        print ">TAG MESSAGE RECEIVED: This is a taged message, to measure latency"
         if msg[2] in self.tagedHeartbeat.keys():
             latency = (float(providerTimestamp) - float(i[0]))
             del self.tagedHeartbeat[msg[2]]
