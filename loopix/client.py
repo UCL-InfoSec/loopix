1adea9f986af43e49332b390d11cff1b4ab356ab
Ania <aniapiotrowska2607@gmail.com>
2016-11-24 21:10:36 +0000

    Client update

--- loopix/client.py
+++ loopix/client.py
@@ -661,10 +661,8 @@ class Client(DatagramProtocol):
                 providerId (int) - identifier of a provider whoes information
                                     we want to pull.
         """
-        provider = None
-        def save_as_ip(IPAddrs, name, port, pkey):
-            provider = format3.Mix(name, port, IPAddrs, pkey)
-            print provider
+        def resolve_to_ip(IPAddrs, name, port, pkey):
+            return format3.Mix(name, port, IPAddrs, pkey)
 
         try:
             db = sqlite3.connect(database)
@@ -674,7 +672,7 @@ class Client(DatagramProtocol):
             fetchData = c.fetchall()
             pData = fetchData.pop()
             #return format3.Mix(str(pData[1]), pData[2], str(pData[3]), petlib.pack.decode(pData[4]))
-            reactor.resolve(str(pData[3])).addCallback(save_as_ip, name=str(pData[1]), port=pData[2], pkey=petlib.pack.decode(pData[4]))
+            provider = reactor.resolve(str(pData[3])).addCallback(save_as_ip, name=str(pData[1]), port=pData[2], pkey=petlib.pack.decode(pData[4]))
             print provider
             return provider
         except Exception, e:
