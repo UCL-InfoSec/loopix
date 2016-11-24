f3f825605fb4147d9debc4fcd55483971739250e
Ania <aniapiotrowska2607@gmail.com>
2016-11-24 21:39:21 +0000

    Client update

--- loopix/client.py
+++ loopix/client.py
@@ -677,7 +677,7 @@ class Client(DatagramProtocol):
             pData = fetchData.pop()
             #return format3.Mix(str(pData[1]), pData[2], str(pData[3]), petlib.pack.decode(pData[4]))
             #reactor.resolve(str(pData[3])).addCallback(save_as_ip, name=str(pData[1]), port=pData[2], pkey=petlib.pack.decode(pData[4]))
-            IP = reactor.resolve(str(pData[3]))
+            IP = yield reactor.resolve(str(pData[3]))
             print IP
             return format3.Mix(str(pData[1]), pData[2], IP, petlib.pack.decode(pData[4]))
         except Exception, e:
