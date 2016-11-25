a08ad1f095513f918d88c45ab202179857d41be2
Ania <aniapiotrowska2607@gmail.com>
2016-11-25 11:52:21 +0000

    Client update

--- loopix/client.py
+++ loopix/client.py
@@ -666,7 +666,7 @@ class Client(DatagramProtocol):
             print "Test print"
             #g = yield reactor.resolve(host)
             #IP = ''.join(g)
-            #defer.returnValue(IP)
+            defer.returnValue("IP")
 
         try:
             db = sqlite3.connect(database)
@@ -679,7 +679,8 @@ class Client(DatagramProtocol):
             #g = yield reactor.resolve(str(pData[3]))
             #IP = ''.join(g)
             IP = defer.inlineCallbacks(resolve_address(str(pData[3])))
-            return format3.Mix(str(pData[1]), pData[2], IP, petlib.pack.decode(pData[4]))
+            print "====================="
+            #return format3.Mix(str(pData[1]), pData[2], IP, petlib.pack.decode(pData[4]))
         except Exception, e:
             print "ERROR: ", str(e)
         finally:
