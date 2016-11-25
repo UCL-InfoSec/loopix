bf567357e8fd605df8bae43a56aa08dea00050fe
Ania <aniapiotrowska2607@gmail.com>
2016-11-25 01:02:29 +0000

    Client update

--- loopix/client.py
+++ loopix/client.py
@@ -662,12 +662,11 @@ class Client(DatagramProtocol):
                 providerId (int) - identifier of a provider whoes information
                                     we want to pull.
         """
-        self.tmpPrv = None
-        def save_as_ip(IPAddrs, name, port, pkey):
-            self.tmpPrv = format3.Mix(name, port, IPAddrs, pkey)
-            self.tmpPrv
-            return self.tmpPrv
-
+        # self.tmpPrv = None
+        # def save_as_ip(IPAddrs, name, port, pkey):
+        #     self.tmpPrv = format3.Mix(name, port, IPAddrs, pkey)
+        #     self.tmpPrv
+        #     return self.tmpPrv
         try:
             db = sqlite3.connect(database)
             c = db.cursor()
@@ -678,10 +677,10 @@ class Client(DatagramProtocol):
             #return format3.Mix(str(pData[1]), pData[2], str(pData[3]), petlib.pack.decode(pData[4]))
             #reactor.resolve(str(pData[3])).addCallback(save_as_ip, name=str(pData[1]), port=pData[2], pkey=petlib.pack.decode(pData[4]))
             vals = yield reactor.resolve(str(pData[3]))
-            IP = defer.returnValue(vals)
-            print IP
-            print type(IP)
-            return format3.Mix(str(pData[1]), pData[2], IP, petlib.pack.decode(pData[4]))
+            defer.returnValue(vals)
+            #print IP
+            #print type(IP)
+            #return format3.Mix(str(pData[1]), pData[2], IP, petlib.pack.decode(pData[4]))
         except Exception, e:
             print "ERROR: ", str(e)
         finally:
