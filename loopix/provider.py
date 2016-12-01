7e2ccd4e7a02c089a055e48edaae2e255f9fcfcd
Ania <aniapiotrowska2607@gmail.com>
2016-12-01 14:00:14 +0000

    Provider update

--- loopix/provider.py
+++ loopix/provider.py
@@ -55,7 +55,7 @@ class Provider(MixNode):
         reactor.callLater(10.0, self.turnOnProcessing)
 
         if self.TAGED_HEARTBEATS == "True":
-            self.d.addCallback(self.self.turnOnTagedHeartbeats)
+            self.d.addCallback(self.turnOnTagedHeartbeats)
         else:
             self.d.addCallback(self.turnOnHeartbeats)
         self.d.addErrback(self.errbackHeartbeats)
