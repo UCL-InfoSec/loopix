e6b223fc795b97bb70c26a6b0a8312cdc779500d
Ania <aniapiotrowska2607@gmail.com>
2017-01-21 12:30:15 +0100

    Mixnode and provider update

--- loopix/provider.py
+++ loopix/provider.py
@@ -70,11 +70,11 @@ class Provider(MixNode):
         
         reactor.callLater(10.0, self.turnOnProcessing)
 
-        if self.TAGED_HEARTBEATS == "True":
-            self.d.addCallback(self.turnOnTagedHeartbeats)
-        else:
-            self.d.addCallback(self.turnOnHeartbeats)
-        self.d.addErrback(self.errbackHeartbeats)
+        #if self.TAGED_HEARTBEATS == "True":
+        #    self.d.addCallback(self.turnOnTagedHeartbeats)
+        #else:
+        #    self.d.addCallback(self.turnOnHeartbeats)
+        #self.d.addErrback(self.errbackHeartbeats)
 
         #self.turnOnReliableUDP()
         self.readInData('example.db')
