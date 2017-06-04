c3b32b0cd8958299b37d0f74eb8010b25bfbe78a
aniampio <anna.piotrowska.15@ucl.ac.uk>
2017-06-05 00:17:16 +0100

    Support formats added

---
+++ loopix/support_formats.py
@@ -0,0 +1,39 @@
+# format 3
+from os import urandom
+from collections import namedtuple
+from binascii import hexlify
+from copy import copy
+import math
+
+from hashlib import sha512, sha1
+import hmac
+
+import msgpack
+
+from petlib.ec import EcGroup
+from petlib.ec import EcPt
+from petlib.bn import Bn
+from petlib.cipher import Cipher
+import base64
+import petlib.pack
+import binascii
+
+import time
+
+Keys = namedtuple('Keys', ['b', 'iv', 'kmac', 'kenc'])
+Mix = namedtuple('Mix', ['name', 'port', 'host', 'pubk', 'group'])
+Provider = namedtuple('Provider', ['name', 'port', 'host', 'pubk'])
+User = namedtuple('User', ['name', 'port', 'host', 'pubk', 'provider'])
+
+Params = namedtuple('Params',
+    ['EXP_PARAMS_LOOPS',
+    'EXP_PARAMS_DROP',
+    'EXP_PARAMS_PAYLOAD',
+    'EXP_PARAMS_DELAY',
+    'DATABASE_NAME',
+    'TIME_PULL',
+    'MAX_DELAY_TIME',
+    'NOISE_LENGTH',
+    'MAX_RETRIEVE'])
+
+Params.__new__.__defaults__ = (None,) * len(Params._fields)
